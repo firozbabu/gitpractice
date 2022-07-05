d9601b5048af8ef7659842bbedb18cb3547b204d
firoz <firozbabu1a0555@gmail.com>
2022-07-05 13:02:01 +0530

    hello and second_file is modified

--- hello.py
+++ hello.py
@@ -1,2 +1,6 @@
 for i in range(10):
 	print('hello world'*i)
+
+
+for j in range(4):
+	print('hello world')
--- second_file.py
+++ second_file.py
@@ -4,3 +4,11 @@ for i in range(len(arr)):
 		if arr[j]>arr[j+1]:
 			arr[j],arr[j+1] = arr[j+1],arr[j]
 print(arr)
+
+
+arr = [6,3,6,0,1,5,5,6,2,23,64,12,434]
+for i in range(len(arr)):
+	for j in range(len(arr)-1):
+		if arr[j]>arr[j+1]:
+			arr[j],arr[j+1] = arr[j+1],arr[j]
+print(arr)
